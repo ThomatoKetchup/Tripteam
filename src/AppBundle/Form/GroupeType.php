bf81b4bf7f5c0f63bf94562aa04b8afcb01d21f0
aasofia <aitahcene.sofia@hotmail.fr>
2018-03-01 01:27:51 +0100

    Page créer un groupe

---
+++ src/AppBundle/Form/GroupeType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GroupeType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('dateDebutG')->add('dateFinG')->add('lieuG');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Groupe'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_groupe';
+    }
+
+
+}
