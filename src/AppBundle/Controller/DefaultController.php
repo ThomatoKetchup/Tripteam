4b06ab0ad8555c955a4240ac7a1849c0f0714c78
ThomatoKetchup <33425389+ThomatoKetchup@users.noreply.github.com>
2018-03-20 10:57:31 +0100

    Ajout de la recherche dans le menu

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,5 +18,16 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    /**
+     * Lists all profil entities.
+     *
+     * @Route("/visiteur", name="visiteur_index")
+     * @Method("GET")
+     */
+    public function visiteurAction()
+    {
+        return $this->render('visiteur/index.html.twig');
     }
 }
